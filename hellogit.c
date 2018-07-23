523a0df61d29cef28f3de849a484724d4137830b
shuizhonghanyue <1459940006@qq.com>
2018-07-23 16:19:14 +0800

    add hellogit

---
+++ hellogit.c
@@ -0,0 +1,13 @@
+ ///
+ /// @file    hellogit.c
+ /// @author  lemon(haohb13@gmail.com)
+ /// @date    2018-07-23 16:17:21
+ ///
+ 
+#include <iostream>
+using std::cout;
+using std::endl;
+int main(){
+	cout<<"hello git"<<endl;
+	return 0;
+}
